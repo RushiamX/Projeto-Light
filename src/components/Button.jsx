165ba7dfbfa03ba5233e857840203b449de0097d
brunofelixf <brunofelixf@gmail.com>
2022-08-11 16:13:29 -0300

    feat: inicia o componente Button

---
+++ src/components/Button.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import './Button.css'
+
+  function Button() {
+  return (
+    <div className="container_btn">
+      <button></button>
+    </div>
+  );
+  }
+
+export default Button;
