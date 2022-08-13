03595d07ec041d5432662e4532a3bc9a06362e66
Thiago Dias <thiago@thiago.com>
2022-08-13 20:58:35 -0300

    header created

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
-
 import './styles.css'
+import headerLogo from '../../assets/images/save_energy.png'
+import iconLeft from '../../assets/images/icon-left-arrow.png'
+import iconExit from '../../assets/images/icon-exit.png'
 
 
 export default function Header() {
   return (
     <div className='Header'>
-      teste
+      <img className="header__icon-left" src={iconLeft} alt="" />
+      <img className="header__logo" src={headerLogo} alt="" />
+      <h2 className='header__title'>Team Light</h2>
+      <img className="header__icon-exit" src={iconExit} alt="" />
+
     </div>
   )
 }
