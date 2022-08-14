1ba207044bdd0899397a7d15858cb048596dae43
Ge Vasconcelos <geminivasconceelos@gmail.com>
2022-08-14 17:23:04 -0300

    feat: adicao do footer para quando o usuario já estiver logado

---
+++ src/components/FooterLogged/index.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './styles.css'
+import greenLeaf from '../../assets/images/energySaving.png'
+import dhLogo from '../../assets/images/dh.png'
+import teamLight from '../../assets/images/teamLight.png'
+
+export default function FooterLogged() {
+  return (
+    <div className='Footer'>
+      <img className='footer__greenLeaf' src={greenLeaf} alt="folha verde" />
+      <div className="footer__title-group">
+      <img className='footer__logo' src={dhLogo} alt="logo dh" />
+      <p className='footer__text'>Powered By Team Light - Front End 2022</p>
+      </div>
+      
+    </div>
+  )
+}
