4a4e11d0624ed11403932436adf5efde686a1d39
Caroline Guimarães dos Santos <carolguimaraes2303@gmail>
2022-08-15 14:28:17 -0300

    ajustando o layout

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import './styles.css'
+import { Link } from 'react-router-dom';
+import sun from "../../assets/images/sun.icon.png";
 import headerLogo from '../../assets/images/save_energy.png'
 import iconLeft from '../../assets/images/icon-left-arrow.png'
 import iconExit from '../../assets/images/icon-exit.png'
@@ -7,12 +9,34 @@ import iconExit from '../../assets/images/icon-exit.png'
 
 export default function Header() {
   return (
-    <div className='Header'>
-      <img className="header__icon-left" src={iconLeft} alt="" />
-      <img className="header__logo" src={headerLogo} alt="" />
-      <h2 className='header__title'>Team Light</h2>
-      <img className="header__icon-exit" src={iconExit} alt="" />
+		<div>
+		<nav className="navbar">
+ 			<img className="nav-logo" src={sun} alt="sun" />
+			 <span className='left'>TEAM LIGHT</span>
+			<div className="nav-icon">
+		
+				<img className="nav-logo" src={headerLogo} alt="sun" />
+			</div>
+			<ul>
+				<li className="nav-item">
+					<Link to="/calculation" className="nav-link">
+					Calculadora
+					</Link>
+				</li>
+				<li className="nav-item">
+					<Link to="/login" className="nav-link">
+						Login
+					</Link>
+				</li>
+			</ul>
+		</nav>
+		<div className='Header'>
+    	<img className="header__icon-left" src={iconLeft} alt="" />
+		<img className="header__logo" src={headerLogo} alt="" />
+		<h2 className='header__title'>Team Light</h2>
+		<img className="header__icon-exit" src={iconExit} alt="" />
 
     </div>
-  )
+		</div>	
+	);
 }
--- src/routes.jsx
+++ src/routes.jsx
@@ -6,16 +6,18 @@ import {
     Outlet
 } from "react-router-dom";
 
+import Login from "./pages/Login";
+import Calculation from "./pages/Form-Calculation";
 import SignUp from "./pages/SingUp";
 import History from "./pages/History";
-
 export default function MyRoutes() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path='/SingUp' element={<SignUp />} />
         <Route path='/History' element={<History />} />
-
+        <Route path='/Login' element={<Login />} />
+        <Route path='/Calculation' element={<Calculation />} />
       </Routes>
     </BrowserRouter>
   )
