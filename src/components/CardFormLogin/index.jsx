118a36aa0256d1e4b36367ad55f2dcc9b512f9ca
Caroline Guimarães dos Santos <carolguimaraes2303@gmail>
2022-08-21 11:59:39 -0300

    integração formulário de cadastro e login

--- src/components/CardFormLogin/index.jsx
+++ src/components/CardFormLogin/index.jsx
@@ -1,16 +1,63 @@
-
 import './styles.css'
+import { Link, useNavigate } from 'react-router-dom';
 
-export default function CardLogin({children}){
-    return(
-        <div className="card__Login">
-            <h2 className='form__title'>Login</h2>
-            <input className='input__login' type="email" placeholder='E-Mail'/>
-            <input className='input__login' type="password" placeholder='Senha'/>
-            <button className='btn__login'><a href='/Calculation'>Entrar</a></button>
-            <a className='text__nao-tenho-conta' href="/SignUp">Não tem conta? Cadastre-se Aqui</a>
-            {children}
-        </div>
+import React from 'react';
+
+export default function CardLogin() {
+    const navigate = useNavigate();
+
+    const [form, setForm] = React.useState({
+        email: '',
+        password: ''
+    });
+
+    const [warning, setWarning] = React.useState({
+        show: false,
+        message: ''
+    });
+
+    const handleChange = (event) => {
+        setForm({
+            ...form,
+            [event.target.name]: event.target.value
+        });
+    }
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(form);
+        let users = [];
+        if (localStorage.getItem('users')) {
+            users = JSON.parse(localStorage.getItem('users'));
+            const user = users.find(u => u.email === form.email && u.password === form.password);
+            if (user) {
+                users.push(user);
+                localStorage.setItem('token', btoa(user.email));
+                setWarning({ show: false, message: '' });
+                navigate('/Calculation');
+            } else {
+                setWarning({ show: true, message: 'Usuário ou senha inválidos' });
+            }
+        } else {
+            setWarning({ show: true, message: 'Não existe usuários cadastrados' });
+            setTimeout(() => {
+                setWarning({ show: false, message: '' });
+            }, 10000);
+        }
+    }
+    return (
+        <form onSubmit={handleSubmit} className='form__login'>
+            <div className="card__Login">
+                <h2 className='form__title'>Login</h2>
+                <input className='input__login' type="email" name='email' placeholder='E-Mail' value={form.email} onChange={handleChange} />
+                <input className='input__login' type="password" name='password' placeholder='Senha' value={form.password}
+                    onChange={handleChange} />
+                <button className='btn__login'>Entrar</button>
+                {warning.show && <span className='warning'>{warning.message}</span>}
+                <Link className='text__nao-tenho-conta' to='/SignUp'>
+                    <span>Não tem conta? Cadastre-se Aqui</span>
+                </Link>     
+            </div>
+        </form>
     )
 
 }
