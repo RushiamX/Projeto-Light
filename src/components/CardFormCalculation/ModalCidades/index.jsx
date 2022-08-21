1ad8e9f61865aff774d1166b88a5537fc7c7b4ee
Thiago Dias <thiago@thiago.com>
2022-08-21 15:30:22 -0300

    iniciado o handleSubmite

--- src/components/CardFormCalculation/ModalCidades/index.jsx
+++ src/components/CardFormCalculation/ModalCidades/index.jsx
@@ -2,19 +2,19 @@ import './styles.css'
 
 export default function ModalCidades(props){
 
-    const handleCity = (cityChosen) => {
-        console.log(cityChosen)
+    const handleCity = (cityChosen,cidade) => {
         props.setForm({
             ...props.form,
             cidade: cityChosen
         });
+        props.setCidadeEscolhida(cidade)
     }
 
     return(
         <div className='modal__cidades'>
             {props.cidades.map(item => (
                     <p className='item__cidade'
-                    onClick={ () => handleCity(item.NAME + " - " + item.STATE)} 
+                    onClick={ () => handleCity(item.NAME + " - " + item.STATE,item)} 
                     key={item.NAME + item.STATE}>{item.NAME} - {item.STATE}</p>
                     ))} 
         </div>
