9bc8eeabfb2d300d0da5287fd8477770ea37e25e
brunofelixf <brunofelixf@gmail.com>
2022-08-22 12:37:08 -0300

    feat: ajusta tipo de ligação para valores correspondentes

--- src/pages/Form-Calculation/calculaPot.js
+++ src/pages/Form-Calculation/calculaPot.js
@@ -3,6 +3,17 @@ function calculaPotencia(consumoMensal, ligacao, irradiacao, temperatura){
   let perdas = 0.24;
   let rendimento = 1 - perdas;
 
+  switch(ligacao){
+    case 'monofásica':
+      ligacao = 30;
+    case 'bifásica':
+      ligacao = 50;
+    case 'trifásica'
+      ligacao = 100;
+  }
+  
+
   resultado = ((consumoMensal - ligacao)/30)/(irradiacao*rendimento)
 
+  return resultado
 }
