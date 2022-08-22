e5e5dd0a10f42a68614cd8c9cee77693bfc6bb96
brunofelixf <brunofelixf@gmail.com>
2022-08-22 12:30:07 -0300

    feat: adiciona fórmula do cálulo de potência

--- src/pages/Form-Calculation/calculaPot.js
+++ src/pages/Form-Calculation/calculaPot.js
@@ -1,6 +1,8 @@
-let consumoMensal, ligacao, irradiacao;
-let perdas = 0.24;
 
-function calculaPotencia(){
-  
+function calculaPotencia(consumoMensal, ligacao, irradiacao, temperatura){
+  let perdas = 0.24;
+  let rendimento = 1 - perdas;
+
+  resultado = ((consumoMensal - ligacao)/30)/(irradiacao*rendimento)
+
 }
