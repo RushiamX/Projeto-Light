a23d69affb6137d2d7ce1a4e3d090784660819f2
Nádia Santos Barbosa <nadiasbsb@gmail.com>
2022-08-23 20:39:01 -0300

    add código na tela Help

--- src/pages/Help/index.jsx
+++ src/pages/Help/index.jsx
@@ -1,9 +1,12 @@
 import './styles.css';
 import Container from '../../components/Container';
 import Waves from '../../components/Waves';
-import Footer from '../../components/Footer';
 import HeaderLogged from '../../components/HeaderLogged';
 import FooterLogged from '../../components/FooterLogged';
+
+import energySaving from '../../assets/images/energySaving.png';
+import logoDh from '../../assets/images/dh.png';
+import teamLight from '../../assets/images/teamLight.png';
 
 
 export default function Help() {
@@ -15,8 +18,8 @@ export default function Help() {
             </div>
             
             <div className='main__help'>
-                <div className='help__descrition'>
-                    <p>
+                <div className='help__description'>
+                    <p className='description__energy'>
                         Lorem ipsum dolor, sit amet consectetur
                         adipisicing elit. Sed, eveniet expedita
                         dolorum, amet ipsum suscipit tenetur, 
@@ -27,14 +30,18 @@ export default function Help() {
                 </div>
 
                 <div className='help__information'>
-                    <p>
-                        Quer saber mais sobre os benefícios de usar esste tipo de energia clicando AQUI.
+                    <p className='information__link'>
+                        Quer saber mais sobre os benefícios de usar esste tipo de energia clicando 
+                        <a href=''>AQUI.</a>
                     </p>
-                </div>    
+                </div> 
+                <div className='help__images'>
+                    <img className='images__energySaving' src={energySaving} alt="folha verde" />
+                    <img className='images__logoDh' src={logoDh} alt="logo Digital House" />
+                    <img className='images__teamLight' src={teamLight} alt="nome do time e da turma" />
+                </div>   
             </div>
-
             <FooterLogged/>
-            {/* <FooterLogged/> */}
         </Container>
     );
 };
