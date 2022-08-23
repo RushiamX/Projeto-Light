d8adfedd3cdd32fbb50cd31aa6b783e14c20dfb7
Thiago Dias <thiago@thiago.com>
2022-08-23 20:17:58 -0300

    retirado os positions nas pages e centralizado os formulários

--- src/pages/Calcutation-Result/index.jsx
+++ src/pages/Calcutation-Result/index.jsx
@@ -3,7 +3,7 @@ import HeaderLogged from '../../components/HeaderLogged';
 import Container from '../../components/Container';
 import FooterLogged from '../../components/FooterLogged';
 import CardFormResult from '../../components/CardFormResult';
-import Waves from '../../components/Waves';
+
 
 export default function Results() {
     return(
