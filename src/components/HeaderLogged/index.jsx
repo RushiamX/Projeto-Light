369772af831724d7ac30b77f8180c7d3f957a560
Nádia Santos Barbosa <nadiasbsb@gmail.com>
2022-08-24 17:49:49 -0300

    finalização dos links e rotas do headerLogged

--- src/components/HeaderLogged/index.jsx
+++ src/components/HeaderLogged/index.jsx
@@ -3,7 +3,8 @@ import './styles.css'
 import headerLogo from '../../assets/images/save_energy.png'
 import btnLeft from '../../assets/images/icon-left-arrow.png'
 import btnLogout from '../../assets/images/icon-exit.png'
-import { Link } from 'react-router-dom'
+import {Link, useNavigate } from 'react-router-dom'
+
 
 
 
@@ -12,6 +13,14 @@ export default function HeaderLogged() {
   const toback = () =>{
     window.history.back()
   }; 
+
+  const navigate = useNavigate();
+  const logout = (e) => {
+    e.preventDefault();
+    sessionStorage.clear();
+
+    navigate('/');
+  }
 
   return (
     <div className='HeaderLogged'>
@@ -22,10 +31,10 @@ export default function HeaderLogged() {
         <img className="header__logo" src={headerLogo} alt="" />
         <p className='header__title'>Team Light</p>
       </div>
-      <Link className="header__btn" to='/Login'>
-        <img src={btnLogout} alt="" className='img__logout'/>
+      <Link className="header__btn" to=''>
+        <img src={btnLogout} className='img__logout' onClick={logout}/>
       </Link>
 
     </div>
   )
-}
+  }
