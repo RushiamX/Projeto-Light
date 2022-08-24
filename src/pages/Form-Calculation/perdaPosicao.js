bd06d6631289cc257bfaadc7aa69acceff4e4de0
brunofelixf <brunofelixf@gmail.com>
2022-08-24 15:24:45 -0300

    feat: ajusta valor de inclinação

--- src/pages/Form-Calculation/perdaPosicao.js
+++ src/pages/Form-Calculation/perdaPosicao.js
@@ -1,3 +1,26 @@
-function perdaPosicao(){
-  
+function perdaPosicao(inclinacao, orientacao){
+
+  switch (inclinacao) {
+    case 5:
+      inclinacao=0;
+      break;
+    case 10:
+      inclinacao=1;
+      break;
+    case 15:
+      inclinacao=2;
+      break;
+    case 20:
+      inclinacao=3;
+      break;
+    case 25:
+      inclinacao=4;
+      break;
+    case 30:
+      inclinacao=5;
+      break;
+      
+    default:
+      break;
+  }
 }
