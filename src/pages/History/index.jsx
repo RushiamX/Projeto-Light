8179edbc9cf6ef709adcc373da4e90ac41a4b09a
Thiago Dias <thiago@thiago.com>
2022-08-28 11:58:34 -0300

    adicionado o modal para inserir dados do cliente

--- src/pages/History/index.jsx
+++ src/pages/History/index.jsx
@@ -3,13 +3,12 @@ import Container from '../../components/Container';
 import HeaderLogged from '../../components/HeaderLogged';
 import FooterLogged from '../../components/FooterLogged';
 import CardFromHistory from '../../components/CardFromHistory';
-import Waves from '../../components/Waves';
 
 export default function History() {
     return (
         <Container>
             <HeaderLogged />
-            <Waves />
+
             <div className="main__history">
                 <CardFromHistory />
             </div>
